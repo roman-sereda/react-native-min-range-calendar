c4f6b5f44131dbd24d8fd4644a0891a3db38fca2
Roman Sereda <roman.sereda@pm.me>
2019-09-13 22:08:40 +0300

    Ref

--- src/Month.js
+++ src/Month.js
@@ -11,7 +11,7 @@ const height = 6 * weekHeight + weekPadding * 5;
 
 class Month extends PureComponent{
   constructor(props){
-    super(props)
+    super(props);
 
     const { userStyles, colors } = this.props;
     let newStyles = helper.mergeStyles(getStyles, userStyles, colors);
@@ -70,10 +70,9 @@ class Month extends PureComponent{
     })
   }
 
-  render(){
-    const { start, end, styles } = this.state;
-    const { month, year, maxDate, minDate, maxRange, minRange, mode, initialDate } = this.props;
-
+  calculateLimits(){
+    const { start } = this.state;
+    const { minRange, maxRange, minDate, maxDate, mode } = this.props;
     // calendar has limits, if date is before minLimit or after maxLimit - it will become unavailable to select
     // limits calculates from minDate / maxDate or minRange / maxRange (false values == no limits)
     // if you have chosen start date, then all dates that before start + minRange or after start + maxRange will become
@@ -92,71 +91,106 @@ class Month extends PureComponent{
       }
     }
 
-    // here we get array of weeks with dates of chosen month
-    let weeks = helper.getMonth(year, month);
-    let weeksCount = weeks.length - 1;
+    return { maxLimit, minLimit };
+  }
 
-    let initialDay = new CustomDate(initialDate);
+  getDefaultParams(date, dayIndex){
+    return {
+      callback: () => this.select(date),
+      key: "day" + date.day + "" + date.month,
+      isMain: dayIndex === 0,
+      testID: dayIndex === 0 ? 'weekend' : 'regular'
+    }
+  }
 
-    // we iterate calendar page, this variables shows if iteration has reached `start` and `end` of selected date range
-    let startReached = false, endReached = false;
-    // just to prevent unnecessary iterations
-    let beforeMinLimit = !!minLimit;
-    // this is the first date in our calendar page(calendar page could also show some days from previous or next month
-    // because we show every week that has at least one day from chosen month)
-    let startDate = { day: weeks[0][0], month, year };
-    // this is how we check if day from previous month
-    if(weeks[0][0] > 7) startDate = helper.subtractMonth(startDate)
-    // if `start` and/or `end` of the range is before start of calendar page then
-    // we have already reached them
-    if(start && start.isBefore(startDate)) startReached = true;
-    if(end && end.isBefore(startDate)) endReached = true;
+  checkOnUnavailability(date, params){
+    if(this.beforeMinLimit){
+      if(this.minLimit.isAfter(date)){
+        params = {...params, callback: () => this.reset(), isUnavailable: true, testID: 'unavailable' };
+      }else{
+        this.beforeMinLimit = true;
+      }
+    }
 
-    return weeks.map((week, weekIndex) => {
+    if(this.initialDay.isEqualTo(date)) params.initial = true;
+
+    if(this.maxLimit && this.maxLimit.isBefore(date)){
+      params = {...params, callback: () => this.reset(), isUnavailable: true, testID: 'unavailable' };
+    }
+
+    return params;
+  }
+
+  checkSelections(date, params){
+    const { end, start } = this.state;
+    // if start reached but end - no, then this day in range
+    if(end !== false && this.startReached && !this.endReached) params = {...params, inRange: true, testID: 'ranged' };
+
+    if(!this.startReached && start && start.isEqualTo(date)){
+      this.startReached = true;
+      params = {...params, isSelected: true, selectedBg: start && end ? "start" : "none", testID: 'selectedLeft' };
+    }
+
+    if(!this.endReached && end && end.isEqualTo(date)){
+      this.endReached = true;
+      params = {...params, isSelected: true, selectedBg: start && end ? "end" : "none", testID: 'selectedRight' };
+    }
+
+    return params;
+  }
+
+  renderDates(){
+    const { month, year } = this.props;
+    const { styles } = this.state;
+
+    return this.weeks.map((week, weekIndex) => {
       return(<View style = {styles.week} key = {week[0] + 'week'}>{
         week.map((day, dayIndex) => {
           let date = { day, month, year };
           // check if day is from previous or next month
           if(weekIndex === 0 && day > 7) date = helper.subtractMonth(date);
-          if(weekIndex === weeksCount && day < 7) date = helper.addMonth(date);
+          if(weekIndex === this.weeksCount && day < 7) date = helper.addMonth(date);
           // isMain: days that should be marked(by default it is Sundays marked by red color)
-          let params = {
-            callback: () => this.select(date),
-            key: "day" + day + "" + month,
-            isMain: dayIndex === 0,
-            testID: dayIndex === 0 ? 'weekend' : 'regular'
-          };
+          let params = this.getDefaultParams(date, dayIndex);
 
-          if(beforeMinLimit){
-            if(minLimit.isAfter(date)){
-              params = {...params, callback: () => this.reset(), isUnavailable: true, testID: 'unavailable' };
-            }else{
-              beforeMinLimit = true;
-            }
-          }
-
-          if(initialDay.isEqualTo(date)) params.initial = true;
-
-          if(maxLimit && maxLimit.isBefore(date)){
-            params = {...params, callback: () => this.reset(), isUnavailable: true, testID: 'unavailable' };
-          }
-          // if start reached but end - no, then this day in range
-          if(end !== false && startReached && !endReached) params = {...params, inRange: true, testID: 'ranged' };
-
-          if(!startReached && start && start.isEqualTo(date)){
-            startReached = true;
-            params = {...params, isSelected: true, selectedBg: start && end ? "start" : "none", testID: 'selectedLeft' };
-          }
-
-          if(!endReached && end && end.isEqualTo(date)){
-            endReached = true;
-            params = {...params, isSelected: true, selectedBg: start && end ? "end" : "none", testID: 'selectedRight' };
-          }
+          params = this.checkOnUnavailability(date, params);
+          params = this.checkSelections(date, params);
 
           return this.days.getDay(date, params);
         })
       }</View>)
     })
+  }
+
+  prepareParams(){
+    const { start, end } = this.state;
+    const { month, year, initialDate } = this.props;
+    const { minLimit, maxLimit } = this.calculateLimits();
+
+    this.maxLimit = maxLimit;
+    this.minLimit = minLimit;
+    // here we get array of weeks with dates of chosen month
+    this.weeks = helper.getMonth(year, month);
+    this.weeksCount = this.weeks.length - 1;
+    this.initialDay = new CustomDate(initialDate);
+    // we iterate calendar page, this variables shows if iteration has reached `start` and `end` of selected date range
+    this.startReached = false, this.endReached = false;
+    // just to prevent unnecessary iterations
+    this.beforeMinLimit = !!minLimit;
+    // this is the first date in our calendar page(calendar page could also show some days from previous or next month
+    // because we show every week that has at least one day from chosen month)
+    this.startDate = { day: this.weeks[0][0], month, year };
+    // this is how we check if day from previous month
+    if(this.weeks[0][0] > 7) this.startDate = helper.subtractMonth(this.startDate)
+    // if `start` and/or `end` of the range is before start of calendar page then
+    // we have already reached them
+    if(start && start.isBefore(this.startDate)) this.startReached = true;
+    if(end && end.isBefore(this.startDate)) this.endReached = true;
+  }
+
+  render(){
+    this.prepareParams();
+    return this.renderDates();
   }
 }
 
